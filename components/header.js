aba0b4fd75e4f4441c14a5e480e147157b8259fc
Waverley Leung <waverley.nyc@gmail.com>
2021-04-07 17:51:07 -0400

    add semantic nav tag to Header

--- components/header.js
+++ components/header.js
@@ -3,16 +3,18 @@ import Link from 'next/link';
 
 export default () => (
     <header>
-        <ul>
-            <li>
-                <Link href="/"><a>Home</a></Link>
-            </li>
-             <li>
-                <Link href="/support"><a>Support</a></Link>
-            </li>
-             <li>
-                <Link href="/wins"><a>Wins</a></Link>
-            </li>
-        </ul>
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/"><a>Home</a></Link>
+                </li>
+                <li>
+                    <Link href="/support"><a>Support</a></Link>
+                </li>
+                <li>
+                    <Link href="/wins"><a>Wins</a></Link>
+                </li>
+            </ul>
+        </nav>
     </header>
 ) 
