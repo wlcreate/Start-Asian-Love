6023909ef0c5232703cae6169363d93135c834c6
emjose <emjose@gmail.com>
2021-04-07 16:09:20 -0400

    adding carousel component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../components/header'
+import Carousel from '../components/Carousel.js'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
   return (
     <div>
         <Header/>
         <h1>Start Asian Love</h1>
+        <div><Carousel/></div>
     </div>
+    
   )
 }
+
