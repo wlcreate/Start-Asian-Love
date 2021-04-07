f0e498388e6fbc42c038f91ea433c571af697196
Waverley Leung <waverley.nyc@gmail.com>
2021-04-07 17:13:00 -0400

    add styling to see prev and next arrows

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../components/header'
 import Carousel from '../components/Carousel.js'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
   return (
