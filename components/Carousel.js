9b263a2b7e42f46fc4f284d640dc2fa3eb4b2559
Waverley Leung <waverley.nyc@gmail.com>
2021-04-07 18:36:13 -0400

    add options and basic styling to Carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -7,31 +7,36 @@ import styles from '../styles/Carousel.module.css'
 const Carousel = () => {
     let settings = {
     dots: true,
+    fade: true,
+    lazyLoad: true,
     infinite: true,
+    autoplay: true,
     speed: 500,
+    autoplaySpeed: 5000,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    pauseOnHover: true
   };
   return (
       <div className={styles.container}> 
         <Slider {...settings}>
-            <div>
+            <div className={styles["slick-slide"]}>
                 <img src="https://images.unsplash.com/photo-1616170969412-d66071bb2da0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjE3NzQ1NjE4&ixlib=rb-1.2.1&q=80&w=1080"/>
             </div>
-            <div>
+            <div className={styles["slick-slide"]}>
+                <h3>Text 1</h3>
+            </div>
+            <div className={styles["slick-slide"]}>
                 <img src="https://images.unsplash.com/photo-1617127894864-e0a367f148e6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjE3NTQ2OTk4&ixlib=rb-1.2.1&q=80&w=1080"/>
             </div>
-            <div>
-                <img src="https://images.unsplash.com/photo-1616606321933-6d410f975e3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjE3NzQyMjc4&ixlib=rb-1.2.1&q=80&w=1080"/>
+            <div className={styles["slick-slide"]}>
+                <h3>Text 2</h3>
             </div>
-            <div>
-                <h3>4</h3>
+            <div className={styles["slick-slide"]}>
+            <img src="https://images.unsplash.com/photo-1616606321933-6d410f975e3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjE3NzQyMjc4&ixlib=rb-1.2.1&q=80&w=1080"/>
             </div>
-            <div>
-                <h3>5</h3>
-            </div>
-            <div>
-                <h3>6</h3>
+            <div className={styles["slick-slide"]}>
+                <h3>Text 3</h3>
             </div>
         </Slider>
       </div>
