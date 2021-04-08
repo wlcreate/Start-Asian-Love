8b49c72ab799764f25d6c49038b0ae0d81ada856
Waverley Leung <waverley.nyc@gmail.com>
2021-04-08 19:39:42 -0400

    add styling to make video player responsive

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,15 +1,17 @@
 import ReactPlayer from 'react-player/lazy'
+import styles from '../styles/VideoPlayer.module.css'
 
 const VideoPlayer = () => {
     return (
-        <div className="player-wrapper">
+        <div className={styles["player-wrapper"]}>
             <ReactPlayer 
-                url='https://www.youtube.com/playlist?list=PLGDu7sAVejJ9QmTuI1OazxL8Tolx-PulB'
+                url='https://www.youtube.com/playlist?list=PLogRWNZ498ETeQNYrOlqikEML3bKJcdcx'
                 // playing={true} // if do not have controls, allows user to play/pause
                 controls={true} // for native player controls
                 // light={true} // just shows video thumbnail and loads full player on click
-                // width='100%'
-                // height='100%'
+                width='100%'
+                height='100%'
+                style={{position: 'absolute', top: 0, left: 0}}
             />
         </div>
     )
