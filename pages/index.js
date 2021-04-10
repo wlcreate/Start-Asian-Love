bfc2018b73cde9dbb30af0b07a72b671fd1c4e53
emjose <emjose@gmail.com>
2021-04-10 13:51:31 -0400

    restyle header into row format with useRouter

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Header from '../components/header'
+import { Header } from '../components/header'
+// import Header from '../components/header'
 import Carousel from '../components/Carousel.js'
 import VideoPlayer from '../components/VideoPlayer.js'
 
@@ -8,7 +9,7 @@ export default function Home() {
   return (
     <div>
         <Header/>
-        <h1>Start Asian Love</h1>
+        {/* <h1>Start Asian Love</h1> */}
         <Carousel/>
         <div>
           <p>
