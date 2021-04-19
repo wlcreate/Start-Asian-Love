f36c0e646089d14b27774ee542d2f9c3758c33d2
emjose <emjose@gmail.com>
2021-04-19 18:54:48 -0400

    create initial  database files and rename for clarification

---
+++ Wins-separate.js
@@ -0,0 +1,61 @@
+// WINS-SEPARATE: Five export defaults.
+
+
+export default art [
+    {
+    "id": 1,
+    "title": "East Side Stories",
+    "url": "https://www.eastsidestories.org",
+    "content": "Dedicated to sharing authentic expressions of the Asian American & Pacific Islander experience through film, media, and education.",
+    "image": "https://res.cloudinary.com/dn1e07eul/image/upload/v1618585126/SAL/image39_oilfzy.png"
+    }
+]
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+export default heritage [
+    {
+        "id": 1,
+        "title": "",
+        "url": "",
+        "content": "",
+        "image": ""
+    }
+]
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+export default contributions [
+    {
+        "id": 1,
+        "title": "",
+        "url": "",
+        "content": "",
+        "image": ""
+    }
+]
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+export default representation [
+    {
+        "id": 1,
+        "title": "",
+        "url": "",
+        "field": "",
+        "content": "",
+        "image": ""
+    }
+]
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+export default cultural [
+    {
+        "id": 1,
+        "title": "",
+        "url": "",
+        "content": "",
+        "image": ""
+    }
+]
