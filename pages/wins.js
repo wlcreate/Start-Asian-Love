c15c4029916dd74bd03228bdc25ff3c59fbee52e
emjose <emjose@gmail.com>
2021-04-19 16:08:44 -0400

    creating initial wins database

--- pages/wins.js
+++ pages/wins.js
@@ -15,8 +15,12 @@ export default function Wins() {
       </div>
       
       <div className={styles.content}>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sit, commodi omnis ab aliquam ea eius corrupti, aspernatur facilis ipsam nihil perferendis asperiores quia consectetur excepturi ducimus laborum, officiis in pariatur deleniti deserunt necessitatibus? Ullam animi adipisci nulla nisi dolore et tempora? Vel quas repellendus alias excepturi quos odit nisi! Et cupiditate minus recusandae architecto ut ea eligendi quidem aspernatur atque nostrum officiis fugiat, dolore, aliquam expedita vel voluptatum earum ab. At labore repellendus odit ad blanditiis nihil laudantium veritatis quam iste, provident tenetur esse ea dolorum libero maxime ex. Doloribus assumenda blanditiis, soluta repudiandae ex ipsum pariatur debitis eum.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic sit, commodi omnis ab aliquam ea eius corrupti, aspernatur facilis ipsam nihil perferendis asperiores quia consectetur excepturi ducimus laborum, officiis in pariatur deleniti deserunt necessitatibus? Ullam animi adipisci nulla nisi dolore et tempora? Vel quas repellendus alias excepturi quos odit nisi! Et cupiditate minus recusandae architecto ut ea eligendi quidem aspernatur atque nostrum officiis fugiat, dolore, aliquam expedita vel voluptatum earum ab. At labore repellendus odit ad blanditiis nihil laudantium veritatis quam iste, provident tenetur esse ea dolorum libero maxime ex. Doloribus assumenda blanditiis, soluta repudiandae ex ipsum pariatur debitis eum.</p>
+         <h3>AAPI Heritage Month</h3> 
+         <h3>Contributions</h3>
+         <h3>Representation</h3>
+         <h3>Cultural Centers</h3>
+         <h3>Art</h3>
+          
       </div>
 
     <Footer/>
