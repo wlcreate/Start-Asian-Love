50d5f97b21c4e030483bdf459e9ad79973d84cb4
emjose <emjose@gmail.com>
2021-04-19 16:08:44 -0400

    creating initial wins database

---
+++ WinsResources.js
@@ -0,0 +1,110 @@
+export default [
+    // ART CATEGORY
+    "art" = [{
+    "id": 1,
+    "title": "East Side Stories",
+    "url": "https://www.eastsidestories.org",
+    "content": "Dedicated to sharing authentic expressions of the Asian American & Pacific Islander experience through film, media, and education.",
+    "image": "https://res.cloudinary.com/dn1e07eul/image/upload/v1618585126/SAL/image39_oilfzy.png"
+    },
+    {
+    "id": 2,
+    "title": "The Peahce Project",
+    "url": "https://www.thepeahceproject.com/",
+    "content": "The Peahce Project is a digital media platform to share underrepresented stories, art, and writing of Asian voices.",
+    "image": "https://res.cloudinary.com/dn1e07eul/image/upload/v1618585125/SAL/image41_vxrbul.png"
+    }],
+
+    // AAPI HERITAGE MONTH CATEGORY
+    "aapi" = [{
+        "id": 1,
+        "title": "",
+        "url": "",
+        "content": "",
+        "image": ""
+    }],
+
+    // CONTRIBUTIONS CATEGORY
+    "contributions" = [{
+        "id": 1,
+        "title": "",
+        "url": "",
+        "field": "",
+        "content": "",
+        "image": ""
+    }],
+
+    // REPRESENTATION CATEGORY
+    "representation" = [{
+        "id": 1,
+        "title": "",
+        "url": "",
+        "field": "",
+        "content": "",
+        "image": ""
+    }],
+
+    // CULTURAL CENTERS CATEGORY
+    "cultural" = [{
+        "id": 1,
+        "title": "",
+        "url": "",
+        "content": "",
+        "image": ""
+    }],
+]
+
+
+export default art [
+    {
+    "id": 1,
+    "title": "East Side Stories",
+    "url": "https://www.eastsidestories.org",
+    "content": "Dedicated to sharing authentic expressions of the Asian American & Pacific Islander experience through film, media, and education.",
+    "image": "https://res.cloudinary.com/dn1e07eul/image/upload/v1618585126/SAL/image39_oilfzy.png"
+    }
+]
+
+
+export default heritage [
+    {
+        "id": 1,
+        "title": "",
+        "url": "",
+        "content": "",
+        "image": ""
+    }
+]
+
+
+export default contributions [
+    {
+        "id": 1,
+        "title": "",
+        "url": "",
+        "content": "",
+        "image": ""
+    }
+]
+
+
+export default representation [
+    {
+        "id": 1,
+        "title": "",
+        "url": "",
+        "field": "",
+        "content": "",
+        "image": ""
+    }
+]
+
+export default cultural [
+    {
+        "id": 1,
+        "title": "",
+        "url": "",
+        "content": "",
+        "image": ""
+    }
+]
