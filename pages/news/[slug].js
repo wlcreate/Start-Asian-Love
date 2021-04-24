346d1c938c5494c59742803f852a4beebed46fab
emjose <emjose@gmail.com>
2021-04-24 01:09:58 -0400

    create initial style and fix typos

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,106 +1,82 @@
-import Head from 'next/head';
 import styles from '../../styles/News.module.css';
 import { useRouter } from 'next/router';
 import { Header } from '../../components/Header'
 
-export const News = ({ articles, pageNumber }) => {
-  const router = useRouter();
-  return articles.length ? (
-    <>
-      <Head>
-        <meta property="og:image" content={articles[0]?.urlToImage} />
-        <meta property="og:description" content={articles[0]?.description} />
-        <meta property="og:title" content={articles[0]?.title + ' and more!'} />
-      </Head>
-      <div className="page-container">
-        <Header />
+export const News = ({ pageNumber, articles }) => {
 
-        <div className={styles.main}>
-          {articles.map((article, index) => (
-            <div key={index} className={styles.post}>
-              <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
-              <p>{article.description}</p>
-              {!!article.urlToImage && <img src={article.urlToImage} />}
+    const router = useRouter();
+
+    return (
+        <div className='page-container'>
+            <Header />
+            <div className={styles.main}>
+                {articles.map((article, index) => (
+                    <div onClick={() => window.open(article.url,'_blank')} key={index} className={styles.post}>
+                        {!!article.urlToImage && <img src={article.urlToImage} alt='news article image' />}
+                        <h1>{article.title}</h1>
+                        <p>{article.description}</p>
+                    </div>
+                ))}
             </div>
-          ))}
+
+            <div className={styles.paginator}>
+                <div 
+                    onClick={() => {
+                        if (pageNumber > 1) {
+                            router.push(`/news/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
+                        };
+                    }}
+                    className={pageNumber === 1 ? styles.disabled : styles.active}>Previous
+                </div>
+
+                <div>Page {pageNumber}</div>
+
+                <div 
+                    onClick={() => {
+                        if (pageNumber < 10) {
+                            router.push(`/news/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
+                        };
+                    }}
+                    className={pageNumber === 10 ? styles.disabled : styles.active}>Next
+                </div>
+
+            </div>
         </div>
-
-        <div className={styles.paginator}>
-          <div
-            className={pageNumber === 1 ? styles.disabled : styles.active}
-            onClick={() => {
-              if (pageNumber > 1) {
-                // As of the current version of Next.js the default behavior for router.push
-                // will leave the scroll where it is, so we have to manually call scrollTo.
-                // This however is being worked on and is fixed in canary.
-                // Show this in tutorial vid:
-                // https://github.com/vercel/next.js/issues/3249
-                router.push(`/news/${pageNumber - 1}`).then(() => window.scrollTo(0, 0));
-              }
-            }}
-          >
-            Previous Page
-          </div>
-
-          <div>#{pageNumber}</div>
-
-          <div
-            className={pageNumber === 10 ? styles.disabled : styles.active}
-            onClick={() => {
-              if (pageNumber < 10) {
-                // As of the current version of Next.js the default behavior for router.push
-                // will leave the scroll where it is, so we have to manually call scrollTo.
-                // This however is being worked on and is fixed in canary.
-                // Show this in tutorial vid:
-                // https://github.com/vercel/next.js/issues/3249
-                router.push(`/news/${pageNumber + 1}`).then(() => window.scrollTo(0, 0));
-              }
-            }}
-          >
-            Next Page
-          </div>
-        </div>
-      </div>
-    </>
-  ) : (
-    <div className="page-container">
-      <Toolbar />
-      <div className={styles.main}>
-        <h1>Oops! No articles for this page</h1>
-      </div>
-    </div>
-  );
+    );
 };
 
 export const getServerSideProps = async pageContext => {
-  const pageNumber = pageContext.query.slug;
+    const pageNumber = pageContext.query.slug;
 
-  if (!pageNumber || pageNumber < 1 || pageNumber > 10) {
+    if (!pageNumber || pageNumber < 1 || pageNumber > 10) {
+        return {
+            props: {
+                articles: [],
+                pageNumber: 1,
+            }
+        }
+    }
+
+    const apiResponse = await fetch(
+        `https://newsapi.org/v2/everything?q="stopasianhate"&pageSize=9&page=${pageNumber}`,
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+            },
+        },
+    );
+    
+    const apiJson = await apiResponse.json();
+
+    const { articles } = apiJson;
+
     return {
-      props: {
-        articles: [],
-        pageNumber: 1,
-      },
+        props: {
+            articles,
+            pageNumber: Number.parseInt(pageNumber),
+        },
     };
-  }
 
-  const apiResponse = await fetch(
-    `https://newsapi.org/v2/everything?q="stopasianhate"&pageSize=9&page=${pageNumber}`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
-      },
-    },
-  ).then(res => res.json());
-
-  const { articles } = apiResponse;
-
-  return {
-    props: {
-      articles: articles,
-      pageNumber: Number.parseInt(pageNumber),
-    },
-  };
-};
+}
 
 export default News;
