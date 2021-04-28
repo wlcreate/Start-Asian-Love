c37a9de222672ecdff181a757b2bc81a7ae8797b
Waverley Leung <waverley.nyc@gmail.com>
2021-04-28 19:16:15 -0400

    fixes SSR of id pages for representation in wins

--- pages/wins/representation/[id].js
+++ pages/wins/representation/[id].js
@@ -1,23 +1,27 @@
-import { useRouter } from 'next/router'
 import { representation as representationResources } from '../../../Wins-separate.js';
 
-const Representive = () => {
-  const router = useRouter()
-  const { id } = router.query
-
-let foundRepresentative = representationResources.find(resource => {
-
-    return resource.id===parseInt(id)})
-console.log(foundRepresentative)
+const Representative = (props) => {
+  const { id, title, image } = props.foundRepresentative
 
   return (
-      <div>
-          <p>Representive: {id}</p>
-          <p>Name: {foundRepresentative.title}</p> 
-          <p>Name: <img src={foundRepresentative.image}></img></p>
-          </div>
+    <div>
+        <p>Representive: {id}</p>
+        <p>Name: {title}</p> 
+        <img src={image}></img>
+    </div>
     ) 
   
 }
 
-export default Representive
+Representative.getInitialProps = ({query}) => {
+
+  let foundRepresentative = representationResources.find(resource => {
+    return resource.id === parseInt(query.id)
+  })
+
+  return {
+    foundRepresentative
+  }
+}
+
+export default Representative
