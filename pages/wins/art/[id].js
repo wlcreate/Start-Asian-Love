155b72ae9154109b90fbcde7c14b6c8a8fc8ad35
Waverley Leung <waverley.nyc@gmail.com>
2021-04-28 19:02:57 -0400

    fixes SSR of id pages for art in wins

--- pages/wins/art/[id].js
+++ pages/wins/art/[id].js
@@ -1,23 +1,26 @@
-import { useRouter } from 'next/router'
 import { art as artResources } from '../../../Wins-separate.js';
 
 
-const Artist = () => {
-  const router = useRouter()
-  const { id } = router.query
-
-let foundArtist = artResources.find(resource => {
-
-    return resource.id===parseInt(id)})
-
+const Artist = (props) => {
   return (
       <div>
-          <p>Artist: {id}</p>
-          <p>Name: {foundArtist.title}</p>
-          <p>Name: <img src={foundArtist.image}></img></p>
-          </div>
+          <p>Artist: {props.foundArtist.id}</p>
+          <p>Name: {props.foundArtist.title}</p>
+          <img src={props.foundArtist.image}></img>
+        </div>
     ) 
   
+}
+
+Artist.getInitialProps = ({query}) => {
+
+  let foundArtist = artResources.find(resource => {
+      return resource.id === parseInt(query.id)
+  })
+
+  return {
+    foundArtist
+  }
 }
 
 export default Artist
