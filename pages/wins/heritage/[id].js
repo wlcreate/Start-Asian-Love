d9737887114c174cbf4e6b2f1ffd9b35f34af7d1
Waverley Leung <waverley.nyc@gmail.com>
2021-04-28 19:13:06 -0400

    fixes SSR of id pages for heritage in wins

--- pages/wins/heritage/[id].js
+++ pages/wins/heritage/[id].js
@@ -1,22 +1,28 @@
 import { useRouter } from 'next/router'
 import { heritage as heritageResources } from '../../../Wins-separate.js';
 
-const Heritage = () => {
-  const router = useRouter()
-  const { id } = router.query
-let foundHeritage = heritageResources.find(resource => {
-
-    return resource.id===parseInt(id)})
-// console.log(representationResources)
+const Heritage = (props) => {
+  const { id, title, image } = props.foundHeritage
 
   return (
-      <div>
-          <p>Heritage: {id}</p>
-          <p>Name: {foundHeritage.title}</p>
-          <p>Name: <img src={foundHeritage.image}></img></p>
-          </div>
-    ) 
+    <div>
+        <p>Heritage: {id}</p>
+        <p>Name: {title}</p>
+        <img src={image}></img>
+    </div>
+  ) 
   
+}
+
+Heritage.getInitialProps = ({query}) => {
+
+  let foundHeritage = heritageResources.find(resource => {
+    return resource.id === parseInt(query.id)
+  })
+
+  return {
+    foundHeritage
+  }
 }
 
 export default Heritage
