03b65000aa1267cbe52d77906df1726fdf0e4bcd
emjose <emjose@gmail.com>
2021-05-03 12:02:12 -0400

    apply style to pages

--- components/WinsResources.js
+++ components/WinsResources.js
@@ -18,13 +18,14 @@ console.log(category)
                             <img className={styles.portrait} src={resource.portrait} alt="resource title"/>
                             <div className={styles.titlebox}><h2>{resource.title}</h2></div>
                             <p className={styles.cardtext}>
-                                {resource.location
+                                {/* {resource.location
                                 ?
                                 <h6>{resource.location}</h6>
                                 :
-                                <></>}
+                                <></>} */}
                             <p className={styles.content}>{resource.content}</p>
-                            <div className={styles.cardbottom}>
+                            <span>Learn more</span>
+                            {/* <div className={styles.cardbottom}>
                                 {resource.field
                                 ?
                                 <div><p>{resource.field}</p></div>
@@ -33,7 +34,7 @@ console.log(category)
                                 :
                                 <></>
                                 }
-                            </div>
+                            </div> */}
                             </p>
                         </div></Link>
                     </div>
@@ -43,3 +44,7 @@ console.log(category)
         </div>
     )
 }
+
+// • Similar to the news page, the hover action triggers the card to expand, with a call to action (Learn more in Line 27) to prompt the user to read/visit the ID page.
+
+// • Ternaries for location and field attributes are commented out for consistent card display in Wins page.
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Header } from '../components/Header'
 import Carousel from '../components/Carousel.js'
 import VideoPlayer from '../components/VideoPlayer.js'
 import { Footer } from '../components/Footer'
-import Link from 'next/link';
+// import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -16,49 +16,72 @@ export default function Home() {
         <Header/>
         <Carousel/>
         <div className={styles.content}>
-          <p>
+
+          <div className={styles.text}>
+            <p>
+              <span>Start Asian Love</span> is a safe space that educates and consolidates resources related to Asian racism, Asian American history, and the Stop Asian Hate movement. Through these resources we hope to raise awareness and encourage you to join the fight and support Asian Americans.
+            </p>
+          </div>
+
+            {/* Should there be a title/call to action above or below the video player? Testing to see if the video player and/or checkout text should be in the middle, beginning, or end of the content text */}
+            <VideoPlayer />
+
+            {/* <Link href='/news/1' passHref>
+              <div className={styles.checkout}>Check out the latest news</div>
+            </Link> */}
+
+          <div className={styles.text}>
             Since the beginning of the pandemic there has been an increase in the number of hate incidents and crimes against Asian Americans. While some have been prosecuted as hate crimes, others have not because it’s hard to prove racist intent. Then there are incidents that have not resulted in official charges at all, such as verbal harassment or coughing/spitting.
-          </p>
-          <h2>
-            3,795
-          </h2>
-          <p>
-            That’s the number of incidents received by the Stop AAPI Hate reporting center from March 19, 2020 to February 28, 2021. Unfortunately, it’s not all. The group notes in their report that this number represents “only a fraction of the number of hate incidents that actually occur”, with the <b><a href="https://stopaapihate.org/2020-2021-national-report/" target="_blank" rel="noopener noreferrer">
-               full report
-            </a></b> showing “how vulnerable Asian Americans are to discrimination, and the types of discrimination they face”.
-          </p>
-        </div>
+          </div>
 
-          {/* Testing to see if the video player and/or checkout text should be in the middle, beginning, or end of the content text */}
-          <VideoPlayer />
+          {/* The factcontainer div wraps around the two fact divs and are side by side: https://smolcss.dev/ */}
 
-          <Link href='/news/1' passHref>
-            <div className={styles.checkout}>Check out the latest news</div>
-          </Link>
+          <div className={styles.factcontainer}>
+            <div className={styles.facts}>
+              <h2>
+                3,795
+              </h2>
+              <p>
+                That’s the number of incidents received by the Stop AAPI Hate reporting center from March 19, 2020 to February 28, 2021.
+                </p>
+                <p> Unfortunately, it’s not all. The group notes in their report that this number represents “only a fraction of the number of hate incidents that actually occur”, with the <b><span><a href="https://stopaapihate.org/2020-2021-national-report/" target="_blank" rel="noopener noreferrer">
+                  full report
+                </a></span></b> showing “how vulnerable Asian Americans are to discrimination, and the types of discrimination they face”.
+              </p>
+            </div>
 
-        <div className={styles.content}>
-          <h2>
-            149%
-          </h2>
-          <p>
-            That’s the surge of anti-Asian hate crimes from 2019 compared to 2020 as reported by The Center for the Study of Hate and Extremism at California State University. The <b><a href="https://www.csusb.edu/sites/default/files/FACT%20SHEET-%20Anti-Asian%20Hate%202020%203.2.21.pdf" target="_blank" rel="noopener noreferrer">
-               report
-            </a></b> also pinpoints the first spike “occurring in March and April amidst a rise in COVID cases and negative stereotyping of Asians relating to the pandemic”. This increase happened while overall hate crimes dropped by 7%.
-          </p>
-          <p>
-            That said, Asian racism and hate is nothing new. It can be traced back to the <span>Chinese Exclusion Act of 1882</span>, which was the first law that restricted immigrants of a specific ethnic group, and seen throughout history all the way to the Atlanta spa shootings that occurred March 16, 2021. Everyday we can expect to wake up to a new report about another incident, and they aren’t stopping.
-          </p>
-          <p>
-            <span>Start Asian Love</span> is a safe space that educates and consolidates resources related to Asian racism, Asian American history, and the Stop Asian Hate movement. Through these resources we hope to raise awareness and encourage you to join the fight and support Asian Americans.
-          </p>
+            <div className={styles.facts}>
+              <h2>
+                149%
+              </h2>
+              <p>
+                That’s the surge of anti-Asian hate crimes from 2019 compared to 2020 as reported by The Center for the Study of Hate and Extremism at California State University.
+                </p> 
+                <p>The <b><span><a href="https://www.csusb.edu/sites/default/files/FACT%20SHEET-%20Anti-Asian%20Hate%202020%203.2.21.pdf" target="_blank" rel="noopener noreferrer">
+                  report
+                </a></span></b> also pinpoints the first spike “occurring in March and April amidst a rise in COVID cases and negative stereotyping of Asians relating to the pandemic”. This increase happened while overall hate crimes dropped by 7%.
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.text}>
+            <p>
+              Asian racism and hate is nothing new. It can be traced back to the <span><a href="https://www.history.com/topics/immigration/chinese-exclusion-act-1882" target="-blank" rel="noopener noreferrer">Chinese Exclusion Act of 1882</a></span>, which was the first law that restricted immigrants of a specific ethnic group, and seen throughout history all the way to the Atlanta spa shootings that occurred March 16, 2021. Everyday we can expect to wake up to a new report about another incident, and they aren’t stopping.
+            </p>
+          </div>
+
         </div>
         
-        <Link href='/news/1' passHref>
+        {/* <Link href='/news/1' passHref>
           <div className={styles.checkout}>Check out the latest news</div>
-        </Link>
+        </Link> */}
 
         <Footer/>
     </div>
     
   )
 }
+
+// • Each paragraph is for now wrapped in their own divs to experiment with arrangement and display of text.
+
+// • The last paragraph is a great intro, and it is currently now the first div below the carousel.
