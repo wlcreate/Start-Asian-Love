62f5e0a8681eff9eff94133893c375fc6a9b3d38
emjose <emjose@gmail.com>
2021-05-04 02:34:16 -0400

    update wins page and footer and import fonts

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css';
 export const Footer = () => {
     return (
         <div className={styles.main}>
-            <p>I am the Footer. © Start Asian Love. All Rights Reserved.</p>
+            <p>Made with 🤍 by <a href="https://github.com/wlcreate" target="_blank" rel="noopener noreferrer">Waverley Leung</a> and <a href="https://github.com/emjose" target="_blank" rel="noopener noreferrer">Emmanuel Jose.</a> All Rights Reserved. <a href="https://github.com/wlcreate/Start-Asian-Love" target="_blank" rel="noopener noreferrer">🤍 Github</a></p>
         </div>
     )
 }
