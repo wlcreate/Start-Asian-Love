77ed7a1b119376351264dd43a4a2ebf4a536c03c
Waverley Leung <waverley.nyc@gmail.com>
2021-05-14 18:00:09 -0400

    adds favicon information and files

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,12 @@ export default function Home() {
         <meta name="description" content="We are a safe space that educates and consolidates resources related to Asian racism, Asian American history, and the Stop Asian Hate movement." />
         <title>Start Asian Love— Combat Hate. Choose Love.</title>
 
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+
         {/* Open Graph/Facebook */}
         <meta property="og:type" content="website" key="ogtype" />
         <meta property="og:url" content="https://start-asian-love.vercel.app/" key="ogurl" />
