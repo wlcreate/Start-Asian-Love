663dfb54fdaa0c498d737eb8892b07df41e90121
Waverley Leung <waverley.nyc@gmail.com>
2021-05-20 19:20:51 -0400

    udpates Home page css with global variables and mobile responsive

--- pages/index.js
+++ pages/index.js
@@ -49,14 +49,16 @@ export default function Home() {
             </p>
           </div>
             
-            <VideoPlayer />
+          <VideoPlayer />
 
             {/* <Link href='/news/1' passHref>
               <div className={styles.checkout}>Check out the latest news</div>
             </Link> */}
 
           <div className={styles.text}>
-            Since the beginning of the pandemic there has been an increase in the number of hate incidents and crimes against Asian Americans. While some have been prosecuted as hate crimes, others have not because it’s hard to prove racist intent. Then there are incidents that have not resulted in official charges at all, such as verbal harassment or coughing/spitting.
+            <p>
+              Since the beginning of the pandemic there has been an increase in the number of hate incidents and crimes against Asian Americans. While some have been prosecuted as hate crimes, others have not because it’s hard to prove racist intent. Then there are incidents that have not resulted in official charges at all, such as verbal harassment or coughing/spitting.
+            </p>
           </div>
 
           {/* The factcontainer div wraps around the two fact divs and are side by side: https://smolcss.dev/ */}
@@ -68,10 +70,10 @@ export default function Home() {
               </h2>
               <p>
                 That’s the number of incidents received by the Stop AAPI Hate reporting center from March 19, 2020 to February 28, 2021.
-                </p>
-                <p> Unfortunately, it’s not all. The group notes in their report that this number represents “only a fraction of the number of hate incidents that actually occur”, with the <b><span><a href="https://stopaapihate.org/2020-2021-national-report/" target="_blank" rel="noopener noreferrer">
+              </p>
+              <p> Unfortunately, it’s not all. The group notes in their report that this number represents “only a fraction of the number of hate incidents that actually occur”, with the <a href="https://stopaapihate.org/2020-2021-national-report/" target="_blank" rel="noopener noreferrer">
                   full report
-                </a></span></b> showing “how vulnerable Asian Americans are to discrimination, and the types of discrimination they face”.
+                </a> showing “how vulnerable Asian Americans are to discrimination, and the types of discrimination they face”.
               </p>
             </div>
 
@@ -81,16 +83,16 @@ export default function Home() {
               </h2>
               <p>
                 That’s the surge of anti-Asian hate crimes from 2019 compared to 2020 as reported by The Center for the Study of Hate and Extremism at California State University.
-                </p> 
-                <p>The <b><span><a href="https://www.csusb.edu/sites/default/files/FACT%20SHEET-%20Anti-Asian%20Hate%202020%203.2.21.pdf" target="_blank" rel="noopener noreferrer">
+              </p> 
+                <p>The <a href="https://www.csusb.edu/sites/default/files/FACT%20SHEET-%20Anti-Asian%20Hate%202020%203.2.21.pdf" target="_blank" rel="noopener noreferrer">
                   report
-                </a></span></b> also pinpoints the first spike “occurring in March and April amidst a rise in COVID cases and negative stereotyping of Asians relating to the pandemic”. This increase happened while overall hate crimes dropped by 7%.
+                </a> also pinpoints the first spike “occurring in March and April amidst a rise in COVID cases and negative stereotyping of Asians relating to the pandemic”. This increase happened while overall hate crimes dropped by 7%.
               </p>
             </div>
           </div>
 
           {/* First row of graphics */}
-          <div className={styles.graphicContainer}>
+          <div className={styles.graphicRowOne}>
             <div className={styles.graphic}><Image src="/Graphics/graphic07.jpg" alt="" loading="lazy" width={374} height={374} layout="responsive"/></div>
             <div className={styles.graphic}><Image src="/Graphics/graphic01.jpg" alt="" loading="lazy" width={374} height={374} layout="responsive"/></div>
             <div className={styles.graphic}><Image src="/Graphics/graphic03.jpg" alt="" loading="lazy" width={374} height={374} layout="responsive"/></div>
@@ -103,11 +105,10 @@ export default function Home() {
             <p>
               So what can we do? Anti-Asian hate won't disappear overnight, and neither will racism as a whole. But what we can do is support, learn, and celebrate each other for our differences and similarities. Know that you are not alone and that by consciously making the choice to choose love instead of hate you are helping combat hate.
             </p>
-            
           </div>
 
           {/* Second row of graphics */}
-          <div className={styles.graphicContainerTwo}>
+          <div className={styles.graphicRowTwo}>
             <div className={styles.graphic}><Image src="/Graphics/graphic04.jpg" alt="" loading="lazy" width={374} height={374} layout="responsive"/></div>
             <div className={styles.graphic}><Image src="/Graphics/graphic05.jpg" alt="" loading="lazy" width={374} height={374} layout="responsive"/></div>
             <div className={styles.graphic}><Image src="/Graphics/graphic09.jpg" alt="" loading="lazy" width={374} height={374} layout="responsive"/></div>
