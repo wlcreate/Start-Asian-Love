1b5d56b76dc1ccbdd69beb91f04048fc64f304b6
Waverley Leung <waverley.nyc@gmail.com>
2021-06-24 18:38:06 -0400

    updates Home styles sheets from css to scss

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home/Home.module.css";
+import styles from "../styles/Home/Home.module.scss";
 import { Header } from "../components/Layout/Header";
 import { Footer } from "../components/Layout/Footer";
 
