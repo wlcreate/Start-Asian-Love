da8bba970f17a7ec7f9cfed7149753db0797f54b
danielkwon89 <danielkwon89@gmail.com>
2021-06-24 15:56:37 -0400

    adding about page

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Header } from "../components/Layout/Header";
+import { Footer } from "../components/Layout/Footer";
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            
+        </div>
+    )
+}
