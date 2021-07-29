741012c985b250c13dd0b9ea486c68c13403ec35
Waverley Leung <waverley.nyc@gmail.com>
2021-07-29 17:03:36 -0400

    adds Team to Sanity Schema

---
+++ sanitydb/schemas/team.js
@@ -0,0 +1,51 @@
+import { array } from "prop-types";
+
+export default {
+  name: "team",
+  title: "Team",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "bio",
+      title: "Bio",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
+        },
+      ],
+    },
+    {
+      name: "social",
+      title: "Social Links",
+      type: "object",
+      fields: [
+        { name: "twitter", type: "url", title: "Twitter" },
+        { name: "linkedIn", type: "url", title: "LinkedIn" },
+        { name: "gitHub", type: "url", title: "GitHub" },
+        { name: "portfolio", type: "url", title: "Portfolio" },
+      ],
+    },
+    {
+      name: "quote",
+      title: "Quote",
+      type: "text",
+    },
+  ],
+};
