7d5454fd43ff88740a754e9243e0d584cf3274e9
Waverley Leung <waverley.nyc@gmail.com>
2021-08-09 17:21:43 -0400

    displaying profile information using Sanity
    Co-authored-by: Co-authored-by: Emmanuel Jose <emjose@gmail.com>
    Co-authored-by: Sandy Dai <sandydai98@gmail.com>

--- components/About/Profile.js
+++ components/About/Profile.js
@@ -1,34 +1,36 @@
 import Image from "next/image";
 import styles from "../../styles/About/Profile.module.scss";
+import { urlFor } from "../../lib/sanity";
 
 import LinkedInIcon from "../../public/Profile/LinkedIn.svg";
 import TwitterIcon from "../../public/Profile/Twitter.svg";
 import GitHubIcon from "../../public/Profile/GitHub.svg";
 import PortfolioIcon from "../../public/Profile/Portfolio.svg";
 
-export const Profile = ({ profileData }) => {
+export const Profile = ({ name, image, social, bio, quote }) => {
+  console.log(social);
   return (
     <div className={styles.profile}>
-      <h3>{profileData.name}</h3>
+      <h3>{name}</h3>
       <div className={styles["profile-pic"]}>
-        <Image src={profileData.image} alt={`Picture of ${profileData.name}`} />
+        <Image src={urlFor(image).url()} alt={`Picture of ${name}`} width={225} height={225} />
       </div>
-      <p className={styles["profile-quote"]}>{profileData.quote}</p>
-      <p className={styles["profile-bio"]}>{profileData.bio}</p>
+      <p className={styles["profile-quote"]}>{quote}</p>
+      <p className={styles["profile-bio"]}>{bio[0].children[0].text}</p>
       <TwitterIcon
-        onClick={() => window.open(`${profileData.social.twitter}`, "_blank")}
+        onClick={() => window.open(`${social.twitter}`, "_blank")}
         className={styles.icon}
       />
       <LinkedInIcon
-        onClick={() => window.open(`${profileData.social.linkedIn}`, "_blank")}
+        onClick={() => window.open(`${social.linkedIn}`, "_blank")}
         className={styles.icon}
       />
       <GitHubIcon
-        onClick={() => window.open(`${profileData.social.gitHub}`, "_blank")}
+        onClick={() => window.open(`${social.gitHub}`, "_blank")}
         className={styles.icon}
       />
       <PortfolioIcon
-        onClick={() => window.open(`${profileData.social.portfolio}`, "_blank")}
+        onClick={() => window.open(`${social.portfolio}`, "_blank")}
         className={styles.icon}
       />
     </div>
--- components/About/ProfileContainer.js
+++ components/About/ProfileContainer.js
@@ -4,10 +4,9 @@ import { Profile } from "./Profile";
 export const ProfileContainer = ({ profiles }) => {
   return (
     <div className={styles["profile-container"]}>
-      {profiles &&
-        profiles.map((profileData) => {
-          return <Profile key={profileData.id} profileData={profileData} />;
-        })}
+      {profiles?.map((profileData) => {
+        return <Profile key={profileData._id} {...profileData} />;
+      })}
     </div>
   );
 };
