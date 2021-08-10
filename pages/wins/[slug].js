fadd1c1ec0c83df272aee5fe07d954bbc340bd53
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-08-10 19:43:28 -0400

    render individual win in wins slug using Sanity
    Co-authored-by: Waverley Leung <waverley.nyc@gmail.com>

--- pages/wins/[slug].js
+++ pages/wins/[slug].js
@@ -4,9 +4,9 @@ import Image from "next/image";
 import styles from "../../styles/Wins/WinsID.module.scss";
 import { Header } from "../../components/Layout/Header";
 import { Footer } from "../../components/Layout/Footer";
-import { sanityClient } from "../../lib/sanity";
+import { urlFor, sanityClient, usePreviewSubscription, PortableText } from "../../lib/sanity";
 
-const winQuery = `*[_type == "wins" && $slug == _id] {
+const winQuery = `*[_type == "wins" && $slug == _id][0] {
   _id,
   category,
   content,
@@ -18,87 +18,95 @@ const winQuery = `*[_type == "wins" && $slug == _id] {
 }`;
 
 export default function Win({ data, preview }) {
-  return <></>;
-  // return (
-  //     <div>
-  //       <Head>
-  //         <meta charset="utf-8" />
-  //         <meta name="viewport" content="width=device-width, initial-scale=1" />
-  //         {/* <meta name="description" content={`Read for more information about ${title}`} /> */}
-  //         {/* <title>Start Asian Love | {data.title}</title> */}
+  const { data: win } = usePreviewSubscription(winQuery, {
+    params: { slug: data?.win._id },
+    initialData: data,
+    enabled: preview,
+  });
 
-  //         {/* Favicon */}
-  //         <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
-  //         <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
-  //         <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
-  //         <link rel="manifest" href="/favicon_io/site.webmanifest" />
+  return (
+    <div>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content={`Read for more information about ${win.title}`} />
+        <title>Start Asian Love | {win.title}</title>
 
-  //         {/* Open Graph/Facebook */}
-  //         <meta property="og:type" content="website" key="ogtype" />
-  //         <meta property="og:url" content="https://www.startalove.com/" key="ogurl" />
-  //         <meta
-  //           property="og:title"
-  //           content="Start Asian Love— Combat Hate. Choose Love."
-  //           key="ogtitle"
-  //         />
-  //         <meta
-  //           property="og:description"
-  //           // content={`Read for more information about ${title}`}
-  //           key="ogdesc"
-  //         />
-  //         <meta
-  //           property="og:image"
-  //           content="https://res.cloudinary.com/dpqssv1uu/image/upload/v1621022082/StartAsianLove-og.jpg"
-  //           key="ogimage"
-  //         />
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
 
-  //         {/* Twitter */}
-  //         <meta property="twitter:card" content="summary_large_image" key="twcard" />
-  //         <meta property="twitter:url" content="https://www.startalove.com/" key="twurl" />
-  //         <meta
-  //           property="twitter:title"
-  //           content="Start Asian Love— Combat Hate. Choose Love."
-  //           key="twtitle"
-  //         />
-  //         <meta
-  //           property="twitter:description"
-  //           // content={`Read for more information about ${title}`}
-  //           key="twdesc"
-  //         />
-  //         <meta
-  //           property="twitter:image"
-  //           content="https://res.cloudinary.com/dpqssv1uu/image/upload/v1621022082/StartAsianLove-og.jpg"
-  //           key="twimage"
-  //         />
-  //       </Head>
-  //       <Header />
-  //       {/* <h2 className={"page-heading"}>{title}</h2> */}
+        {/* Open Graph/Facebook */}
+        <meta property="og:type" content="website" key="ogtype" />
+        <meta property="og:url" content="https://www.startalove.com/" key="ogurl" />
+        <meta
+          property="og:title"
+          content="Start Asian Love— Combat Hate. Choose Love."
+          key="ogtitle"
+        />
+        <meta
+          property="og:description"
+          content={`Read for more information about ${win.title}`}
+          key="ogdesc"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dpqssv1uu/image/upload/v1621022082/StartAsianLove-og.jpg"
+          key="ogimage"
+        />
 
-  //       <div className={styles.container}>
-  //         <Image
-  //           src={image}
-  //           // alt={`Image of ${title}`}
-  //           loading="lazy"
-  //           width={616}
-  //           height={462}
-  //           layout="responsive"
-  //         />
-  //         <p>{content}</p>
-  //         <a href={url} target="_blank" rel="noopener noreferrer">
-  //           {/* Discover more about {title} */}
-  //         </a>
-  //       </div>
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" key="twcard" />
+        <meta property="twitter:url" content="https://www.startalove.com/" key="twurl" />
+        <meta
+          property="twitter:title"
+          content="Start Asian Love— Combat Hate. Choose Love."
+          key="twtitle"
+        />
+        <meta
+          property="twitter:description"
+          content={`Read for more information about ${win.title}`}
+          key="twdesc"
+        />
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/dpqssv1uu/image/upload/v1621022082/StartAsianLove-og.jpg"
+          key="twimage"
+        />
+      </Head>
+      <Header />
+      <h2 className={"page-heading"}>{win.title}</h2>
 
-  //       <Link href="/wins" scroll={false}>
-  //         <a className={styles.backToWins}>Back To Wins</a>
-  //       </Link>
-  //       <Footer />
-  //     </div>
-  // );
+      <div className={styles.container}>
+        {!urlFor(win.image).url() ? (
+          <></>
+        ) : (
+          <Image
+            src={urlFor(win.image).url()}
+            alt={`Image of ${win.title}`}
+            loading="lazy"
+            width={616}
+            height={462}
+            layout="responsive"
+          />
+        )}
+        <PortableText blocks={win.content} />
+        <a href={win.url} target="_blank" rel="noopener noreferrer">
+          Discover more about {win.title}
+        </a>
+      </div>
+
+      <Link href="/wins" scroll={false}>
+        <a className={styles.backToWins}>Back To Wins</a>
+      </Link>
+      <Footer />
+    </div>
+  );
 }
 
 export async function getStaticPaths() {
-  // console.log("SLUGG", slug);
   const paths = await sanityClient.fetch(
     `*[_type == "wins" && defined(_id)]{
       "params": {
